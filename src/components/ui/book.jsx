46f8f305edeab22c6fe5b6ba35c344ee4027dbd3
antoniozcasas <antoniozcasas@gmail.com>
2025-03-15 15:39:25 -0500

    second commit

---
+++ src/components/ui/book.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Book = ({ book }) => {
+  return (
+    <div className="book">
+      <a href="">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </a>
+      <div className="book__title">
+        <a href="/" className="book__title--link">
+          {book.title}
+        </a>
+      </div>
+      <div className="book__ratings">
+        {
+            new Array(5).fill(0).map((_, i) => (
+              <FontAwesomeIcon
+                key={i}
+                icon={i + 1 <= book.rating ? "star" : "star-half-alt"}
+                className="book__ratings--icon"
+              />
+            ))
+        }
+      </div>
+      <div className="book__price">
+        {book.salePrice ? (
+          <>
+            <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>$
+            {book.salePrice.toFixed(2)}
+          </>
+        ) : (
+            <>${book.originalPrice.toFixed(2)}</>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Book;
